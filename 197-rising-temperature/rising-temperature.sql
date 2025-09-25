72ebeef377871f6e4434649fc4903d506cb1ebb9
Subham <151402309+subh9m@users.noreply.github.com>
2025-09-25 15:20:00 +0530

    Time: 3086 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+SELECT W1.id 
+from 
+Weather as W1
+join Weather as W2
+on datediff(day, W2.recordDate, W1.recordDate) = 1
+where W1.temperature > W2.temperature;
+
