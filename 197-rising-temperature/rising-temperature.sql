885d503a0272a47273f3f43c4bb253028979677c
Subham <151402309+subh9m@users.noreply.github.com>
2025-09-25 15:38:22 +0530

    Time: 464 ms (66.94%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,8 +1,9 @@
-/* Write your T-SQL query statement below */
+# Write your MySQL query statement below
+
 select W1.id
 from Weather as W1
 join 
 Weather as W2
 on 
-datediff(day, W2.recordDate, W1.recordDate) = 1
+timestampdiff(day, W2.recordDate, W1.recordDate) = 1
 where W1.temperature > W2.temperature
