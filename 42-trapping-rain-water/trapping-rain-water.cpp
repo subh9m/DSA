51c8e740ef799a3832d122dabf6eafd29dea7326
Subham <151402309+subh9m@users.noreply.github.com>
2025-09-26 11:59:45 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (60.58%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,37 +2,30 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        vector<int>left_max(n, 0);
-        vector<int>right_max(n, 0);
-        //setting two pointers for comparison...
-        int curr_left_max = height[0];
-        int curr_right_max = height[n-1];
-
-        //calculating left max array...
-        for(int i = 0; i < n; i++){
-            if(height[i] > curr_left_max){
-                left_max[i] = height[i];
-                curr_left_max = max(curr_left_max, height[i]);
-            } else {
-                left_max[i] = curr_left_max;
-            }
-        }
-
-        //calculating right max array...
-        for(int i = n - 1; i >= 0; i--) {
-            if(height[i] > curr_right_max){
-                right_max[i] = height[i];
-                curr_right_max = max(curr_right_max, height[i]);
-            } else{
-                right_max[i] = curr_right_max;
-            }
-        }
-
+        //two pointers left and right...
+        int left = 0;
+        int right = n - 1;
+        int left_max = 0, right_max = 0;
         int trappedWater = 0;
-        for(int i = 0; i < n; i++) {
-            int water = min(left_max[i], right_max[i]);
-            trappedWater += water - height[i];
+        
+        while(left < right) {
+            if(height[left] > height[right]){
+                if(height[right] >= right_max){
+                    right_max = height[right];
+                } else{
+                    trappedWater += right_max - height[right];
+                }
+                right--;
+            } else{
+                if(height[left] >= left_max){
+                    left_max = height[left];
+                } else{
+                    trappedWater += left_max - height[left];
+                }
+                left++;
+            }
         }
+
         return trappedWater;
     }
 };
