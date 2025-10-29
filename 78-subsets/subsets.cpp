b4663c653362f1db1a02f92a035d5829572aff95
Subham <151402309+subh9m@users.noreply.github.com>
2025-10-29 19:47:36 +0530

    Time: 3 ms (30.59%) | Memory: 12.9 MB (23.91%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,20 +1,25 @@
 class Solution {
-public:
-    void generateSubsets(int index, int n, vector<int>&list, vector<vector<int>>&ans, vector<int>&nums){
-        if(index == n){
-            ans.push_back(list);
-            return;
+private: 
+            void findSum(int index, int n, vector<int>&list, vector<int>&nums,
+                        vector<vector<int>>&ans){
+                    if(index == n){
+                        ans.push_back(list);
+                        return;
+                    }
+
+                    findSum(index + 1, n, list, nums, ans);
+                    list.push_back(nums[index]);
+                    findSum(index + 1, n, list, nums, ans);
+                    list.pop_back();
         }
-        generateSubsets(index+1, n, list, ans, nums);
-        list.push_back(nums[index]);
-        generateSubsets(index + 1, n, list, ans, nums);
-        list.pop_back();
-    }
+public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>>ans;
         vector<int> list;
-        generateSubsets(0, n, list, ans, nums);
+
+        findSum(0, n, list, nums, ans);
+
         return ans;
     }
 };
