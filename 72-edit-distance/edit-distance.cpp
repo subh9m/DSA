ef80d909713456c1ad3485f2a0798ac3a5b957ae
Subham <151402309+subh9m@users.noreply.github.com>
2025-11-11 10:51:43 +0530

    Time: 11 ms (30.30%) | Memory: 14.5 MB (15.58%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,23 +1,21 @@
 class Solution {
 private:
-    int solve(string &a, string &b, int i, int j, vector<vector<int>>&dp){
-        if(i == a.size()) return b.size() - j;
+    int solve(string &a, string &b, int m, int n, vector<vector<int>>&dp){
+        if(m == 0) return n;
+        if(n == 0) return m;
+        if(dp[m][n] != -1) return dp[m][n];
+        if(a[m-1] == b[n-1]) return dp[m][n] = solve(a, b, m-1, n-1, dp);
+    
+            int insert = 1 + solve(a, b, m, n-1, dp);
+            int del = 1 + solve(a, b, m-1, n, dp);
+            int replace = 1 + solve(a, b, m-1, n-1, dp);
+    
 
-        if(j == b.size()) return a.size() - i;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(a[i] == b[j]) return dp[i][j] = solve(a, b, i+1, j+1, dp);
-
-        int insert = 1 + solve(a, b, i, j+1, dp);
-        int del = 1+ solve(a, b, i+1, j, dp);
-        int replace = 1 + solve(a, b, i+1, j+1, dp);
-
-        return dp[i][j] = min({insert, del, replace});
+        return dp[m][n] = min({insert, del, replace}); 
     }
 public:
     int minDistance(string word1, string word2) {
-        vector<vector<int>>dp(word1.size() , vector<int>(word2.size(), -1));
-        return solve(word1, word2, 0, 0, dp);
+        vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1, -1));
+        return solve(word1, word2, word1.size(), word2.size(), dp);
     }
 };
