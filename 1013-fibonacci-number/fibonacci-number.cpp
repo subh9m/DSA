43becd10df57ca6c9fabed1994dd10b277dfd4df
Subham <151402309+subh9m@users.noreply.github.com>
2025-12-09 21:04:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (11.32%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,20 +1,16 @@
-//memoisation approach.
+//tabulation
+
 class Solution {
 public:
-    vector<int>dp;
-
-    int solve(int n){
-        if(n <= 1){
-            return n;
-        }
-        if(dp[n] != -1){
-            return dp[n];
-        }
-
-        return dp[n] =  solve(n-1) + solve(n-2);
-    }
     int fib(int n) {
-        dp.assign(n + 1, -1);
-        return solve(n);
+        if(n<=1) return n;
+        vector<int>dp(n+1);
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
