6e36b53e1ae5305030820cd0860f8534c8d7d4c7
Subham <151402309+subh9m@users.noreply.github.com>
2026-01-10 16:16:54 +0530

    Time: 25 ms (88.85%) | Memory: 56.3 MB (8.98%) - LeetSync

---
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int MOD = 1e9 + 7;
+    int numSubseq(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        vector<long long>computed(n, 1);
+
+        for(int i = 1; i < n; i++){
+            computed[i] = 2 * computed[i-1] % MOD;
+        }
+
+        sort(nums.begin(), nums.end());
+
+        int l = 0;
+        int r = n - 1;
+        long long ans = 0;
+        while(l <= r){
+            if(nums[r] + nums[l] <= target){
+                ans += computed[r-l];
+                l++;
+            } else r--;
+        }
+
+        return ans % MOD;
+    }
+};
