f2c2e48ef706ec5f513cd295bd2128bd939626da
Subham <151402309+subh9m@users.noreply.github.com>
2026-01-14 09:41:48 +0530

    Time: 3 ms (79.37%) | Memory: 11.7 MB (73.95%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -3,11 +3,13 @@ public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
         if(n == 0) return 0;
+        if(n == 1) return 1;
+
+        vector<int>lastSeen(256, -1);
         int left = 0;
         int maxLen = 1;
-        vector<int> lastSeen(256, -1);
-        for(int right = 0; right < n; right++) {
-            if(lastSeen[s[right]] >= left) {
+        for(int right = 0; right < n; right++){
+            if(lastSeen[s[right]] >= left){
                 left = lastSeen[s[right]] + 1;
             }
             lastSeen[s[right]] = right;
