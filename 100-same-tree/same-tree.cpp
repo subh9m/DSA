a6f21b77019066d89f2b4e22923c4adb9c5ff23e
Subham <151402309+subh9m@users.noreply.github.com>
2026-02-26 11:25:16 +0530

    Time: 2 ms (0.87%) | Memory: 13 MB (12.76%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -16,8 +16,8 @@ public:
             return (p == q);
         }
 
-        return (p->val == q -> val) && 
-                isSameTree(p->left, q-> left) &&
-                isSameTree(p-> right , q-> right);
+        return (p->val == q-> val)&&
+               isSameTree(p->left, q->left)&&
+               isSameTree(p->right, q->right);
     }
 };
