2e975a35f908804445856a5d4b33964123fc818a
Subham <rajshubham302@gmail.com>
2026-03-11 10:26:26 +0530

    Time: 3 ms (39.64%) | Memory: 15.4 MB (88.48%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,38 +12,32 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector<vector<int>>ans;
-
-        if(root == nullptr) return ans;
-
-        queue<pair<TreeNode*, pair<int, int>>>q;
+        
         map<int, map<int, priority_queue<int, vector<int>, greater<int>>>>mpp;
+        queue<pair<TreeNode*, pair<int, int>>>q;
         q.push({root, {0, 0}});
         while(!q.empty()){
             auto it = q.front();
             q.pop();
-
             TreeNode* node = it.first;
             int x = it.second.first;
             int y = it.second.second;
 
             mpp[x][y].push(node->val);
 
-            if(node->left != nullptr){
-                q.push({node->left, {x-1, y+1}});
-            }
+            if(node->left)q.push({node->left, {x-1, y+1}});
 
-            if(node->right != nullptr){
-                q.push({node->right, {x+1, y+1}});
-            }
+            if(node->right)q.push({node->right, {x+1, y+1}});
         }
 
-        for(auto &q: mpp){
+        vector<vector<int>>ans;
+
+        for(auto &p: mpp){
             vector<int>column;
-            for(auto &p: q.second){
-                while(!p.second.empty()){
-                    column.push_back(p.second.top());
-                    p.second.pop();
+            for(auto &a: p.second){
+                while(!a.second.empty()){
+                    column.push_back(a.second.top());
+                    a.second.pop();
                 }
             }
             ans.push_back(column);
