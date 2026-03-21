e0e2ea133428c9423f06db6a8f59b11fc86a21b2
Subham <rajshubham302@gmail.com>
2026-03-22 03:44:29 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (67.06%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -6,22 +6,21 @@ public:
         int high = n - 1;
 
         while(low <= high){
-            int mid = low + (high - low) /2;
+            int mid = low + (high - low)/2;
             if(nums[mid] == target) return mid;
-            else if(nums[low] <= nums[mid]){
+
+            if(nums[low] <= nums[mid]){
                 if(nums[low] <= target && target <= nums[mid]){
                     high = mid - 1;
                 } else{
-                    low = mid + 1;
+                    low = mid +1;
                 }
             } else{
-                
-                    if(nums[mid] <= target && target <= nums[high]){
-                        low = mid +1;
-                    } else{
-                        high = mid - 1;
-                    }
-                
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid +1;
+                } else{
+                    high = mid -1;
+                }
             }
         }
         return -1;
