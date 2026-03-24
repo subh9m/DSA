716e9aad6da9e972bcfb4de16846d89b531d50b8
Subham <rajshubham302@gmail.com>
2026-03-24 10:29:34 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (92.01%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,37 +12,40 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        
+        vector<vector<int>>result;
+
+        if(root == nullptr)return result;
+
         map<int, map<int, priority_queue<int, vector<int>, greater<int>>>>mpp;
         queue<pair<TreeNode*, pair<int, int>>>q;
         q.push({root, {0, 0}});
+
         while(!q.empty()){
-            auto it = q.front();
+            TreeNode* node = q.front().first;
+            int x = q.front().second.first;
+            int y = q.front().second.second;
             q.pop();
-            TreeNode* node = it.first;
-            int x = it.second.first;
-            int y = it.second.second;
 
             mpp[x][y].push(node->val);
 
-            if(node->left)q.push({node->left, {x-1, y+1}});
-
-            if(node->right)q.push({node->right, {x+1, y+1}});
+            if(node->left){
+                q.push({node->left, {x-1, y+1}});
+            }
+            if(node->right){
+                q.push({node->right, {x+1, y+1}});
+            }
         }
 
-        vector<vector<int>>ans;
-
-        for(auto &p: mpp){
+        for(auto &it: mpp){
             vector<int>column;
-            for(auto &a: p.second){
-                while(!a.second.empty()){
-                    column.push_back(a.second.top());
-                    a.second.pop();
+            for(auto &k: it.second){
+                while(!k.second.empty()){
+                    column.push_back(k.second.top());
+                    k.second.pop();
                 }
             }
-            ans.push_back(column);
+            result.push_back(column);
         }
-
-        return ans;
+        return result;
     }
 };
