e88cb370e31198dc1a2ee07cbf37bb682e3e7690
Subham <rajshubham302@gmail.com>
2026-03-25 09:54:06 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (34.47%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,16 +11,17 @@
  */
 class Solution {
 public:
-    void func(TreeNode* root, vector<int>&ans){
-        if(root == nullptr)return;
-
-        func(root->left, ans);
-        ans.push_back(root->val);
-        func(root->right, ans);
+    void func(TreeNode* root, vector<int>&inorder){
+        if(root == nullptr){
+            return;
+        }
+        if(root->left) func(root->left, inorder);
+        inorder.push_back(root->val);
+        if(root->right) func(root->right, inorder);
     }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        func(root, ans);
-        return ans;
+        vector<int>inorder;
+        func(root, inorder);
+        return inorder;
     }
 };
