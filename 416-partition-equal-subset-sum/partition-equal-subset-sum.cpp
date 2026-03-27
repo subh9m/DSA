c08aba09fdf812c0a21a34402248b67b7f08df04
Subham <rajshubham302@gmail.com>
2026-03-27 10:41:06 +0530

    Time: 183 ms (16.11%) | Memory: 111.3 MB (14.65%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    bool func(int ind, int target, vector<int>&nums, vector<vector<int>>&dp){
-        if(target == 0) return true;
-
-        if(ind == 0){
-            return nums[ind] == target;
+    bool func(int index, int target, vector<int>&nums, vector<vector<int>>&dp){
+        if(index == nums.size() && target != 0){
+            return false;
         }
 
-        if(dp[ind][target] != -1) return dp[ind][target];
+        if(target == 0)return true;
 
-        bool notTaken = func(ind-1, target, nums, dp);
-        bool taken = false;
-        if(nums[ind] <= target){
-            taken = func(ind-1, target - nums[ind], nums, dp);
+        if(dp[index][target] != -1)return dp[index][target];
+
+        bool notPick = func(index + 1, target, nums, dp);
+        bool pick = false;
+        if(nums[index] <= target){
+            pick = func(index + 1, target - nums[index], nums, dp);
         }
-        return dp[ind][target] = taken || notTaken;
+
+        return dp[index][target] = (pick || notPick);
     }
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int sum = 0;
-
-        for(int i = 0; i < n; i++){
-            sum += nums[i];
-        }
+        for(auto it: nums)sum += it;
 
         if(sum % 2){
             return false;
         } else{
-            int k = sum / 2;
-            vector<vector<int>>dp(n, vector<int>(sum+1, -1));
-            return func(n-1, k, nums, dp);
+            int k = sum/2;
+            vector<vector<int>>dp(n+1, vector<int>(k+1, -1));
+            return func(0, k, nums, dp);
         }
     }
 };
