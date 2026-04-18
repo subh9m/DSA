4c68f5470b80786b0bae073d5a12c8959c84f2e1
Subham <rajshubham302@gmail.com>
2026-04-18 23:18:15 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (60.97%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
+        int n1 = nums1.size();
+        int n2 = nums2.size();
 
-        vector<int>merged;
-        int i = 0;
-        int j = 0;
-        while(i < n && j < m){
-            if(nums1[i] < nums2[j]){
-                merged.push_back(nums1[i++]);
+        if(n1 > n2)return findMedianSortedArrays(nums2, nums1);
+
+        int n = n1+n2;
+
+        int left = (n1 +n2 +1)/2;
+
+        int low = 0;
+        int high = n1;
+        while(low <= high){
+            int mid1 = (low + high) >> 1;
+            int mid2 = left - mid1;
+
+            int l1 = (mid1 > 0)?nums1[mid1-1] : INT_MIN; 
+            int r1 = (mid1 < n1)? nums1[mid1] : INT_MAX;
+            int l2 = (mid2 > 0)? nums2[mid2-1]: INT_MIN;
+            int r2 = (mid2 < n2)? nums2[mid2] : INT_MAX;
+
+            if(l1 <= r2 && l2 <= r1){
+                if(n % 2  == 1){
+                    return max(l1, l2);
+                }else{
+                    return (max(l1, l2) + min(r1, r2)) / 2.0;
+                }
+            } else if(l1 > r2){
+                high = mid1 -1;
             } else{
-                merged.push_back(nums2[j++]);
+                low = mid1 + 1;
             }
         }
-
-        while(i < n) merged.push_back(nums1[i++]);
-        while(j < m) merged.push_back(nums2[j++]);
-
-        int merged_size = merged.size();
-
-        if(merged_size % 2 != 0){
-            int mid = merged_size / 2;
-            return (double)merged[mid];
-        } else{
-            int mid1 = merged_size / 2;
-            int mid2 = (merged_size / 2) - 1;
-            return ((double)merged[mid1] + (double)merged[mid2]) / 2.0;
-        }
+        return 0;
     }
 };
