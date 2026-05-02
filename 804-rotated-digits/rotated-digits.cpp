f3a03dbb2d7d631d1642d1d7c9a9d2c488c9abf5
Subham <rajshubham302@gmail.com>
2026-05-02 21:21:59 +0530

    Time: 61 ms (5.79%) | Memory: 7.7 MB (83.68%) - LeetSync

---
+++ 804-rotated-digits/rotated-digits.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int rotatedDigits(int n) {
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+
+            int currentNum = i;
+
+            bool flag = false;
+            while (currentNum > 0) {
+                int currentDigit = currentNum % 10;
+
+                if (!flag && (currentDigit == 2 || currentDigit == 5 ||
+                              currentDigit == 6 || currentDigit == 9)) {
+                    flag = true;
+                }
+                if ((currentDigit == 4 || currentDigit == 7 ||
+                     currentDigit == 3)) {
+                    flag = false;
+                    break;
+                }
+                currentNum /= 10;
+            }
+
+            if (flag) {
+                cout << i << endl;
+
+                result++;
+            }
+        }
+        return result;
+    }
+};
