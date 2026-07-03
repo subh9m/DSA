ce3f15542d490409cd46f3a8778db1e96894c1d7
Subham <rajshubham302@gmail.com>
2026-07-03 07:51:34 +0530

    Time: 3 ms (40.87%) | Memory: 15.6 MB (83.07%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -14,38 +14,42 @@ public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         vector<vector<int>>result;
 
-        if(root == nullptr)return result;
+        if(root == nullptr) return result;
 
-        map<int, map<int, priority_queue<int, vector<int>, greater<int>>>>mpp;
-        queue<pair<TreeNode*, pair<int, int>>>q;
+        map<int, map<int, priority_queue<int, vector<int>, greater<int>>>>nodesMap;
+
+        queue<pair<TreeNode*, pair<int,int>>>q;
         q.push({root, {0, 0}});
 
         while(!q.empty()){
-            TreeNode* node = q.front().first;
-            int x = q.front().second.first;
-            int y = q.front().second.second;
+            auto it = q.front();
             q.pop();
+            TreeNode* node = it.first;
+            int x = it.second.first;
+            int y = it.second.second;
 
-            mpp[x][y].push(node->val);
+            nodesMap[x][y].push(node->val);
 
             if(node->left){
                 q.push({node->left, {x-1, y+1}});
             }
+
             if(node->right){
                 q.push({node->right, {x+1, y+1}});
             }
         }
 
-        for(auto &it: mpp){
-            vector<int>column;
-            for(auto &k: it.second){
-                while(!k.second.empty()){
-                    column.push_back(k.second.top());
-                    k.second.pop();
+        for(auto &it: nodesMap){
+            vector<int>level;
+            for(auto &iter: it.second){
+                while(!iter.second.empty()){
+                    level.push_back(iter.second.top());
+                    iter.second.pop();
                 }
             }
-            result.push_back(column);
+            result.push_back(level);
         }
+
         return result;
     }
 };
