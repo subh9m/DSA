10be1be9473e320ecc3254ff6e7eb569dc28c481
Subham <rajshubham302@gmail.com>
2026-07-09 19:18:19 +0530

    Time: 7 ms (61.04%) | Memory: 13.3 MB (41.69%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,23 +1,29 @@
 class Solution {
-private:
-    int solve(string &a, string &b, int m, int n, vector<vector<int>>&dp){
-        //if any one of them becomes zero, return the other.
-        if(m == 0) return n;
-        if(n == 0) return m;
-        if(dp[m][n] != -1) return dp[m][n];
-        if(a[m-1] == b[n-1]) return dp[m][n] = solve(a, b, m-1, n-1, dp);
-    
-            int insert = 1 + solve(a, b, m, n-1, dp);
-            int del = 1 + solve(a, b, m-1, n, dp);
-            int replace = 1 + solve(a, b, m-1, n-1, dp);
-    
-
-        return dp[m][n] = min({insert, del, replace}); 
-    }
 public:
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1, -1));
-        //recursion starting from backward indices.
-        return solve(word1, word2, word1.size(), word2.size(), dp);
+        int n = word1.size();
+        int m = word2.size();
+
+        vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
+
+        for(int i = 0; i <= n; i++){
+            dp[i][0] = i;
+        }
+
+        for(int j = 0; j <= m; j++){
+            dp[0][j] = j;
+        }
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 1 + min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]});
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
